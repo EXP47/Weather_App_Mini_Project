47f69b057c21f5ab69240bb28dab51921c3511c1
EXP47 <exploder47z@gmail.com>
2025-03-02 02:30:20 -0500

    Finished Project

--- weather-app-clientside/src/App.js
+++ weather-app-clientside/src/App.js
@@ -1,38 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
-
-
-const api = {
-  key: "fa9f5178ee72ca1e2a6a88cd6fa19436",
-  base: "https://api.openweathermap.org/data/2.5/",
-}
+import React, { useState } from 'react'
+import axios from 'axios'
 
 function App() {
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+      setLocation('')
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* HEADER */}
-        <h1>Weather App</h1>
-      {/* Search Box */}
-      <div className="search-box">
-        <input type="text" className="search-bar" placeholder="Search..." />
+    <div className="app">
+      <div className="search">
+        <input
+          value={location}
+          onChange={event => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder='Enter Location'
+          type="text" />
       </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+          <div className="temp">
+            {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
+          </div>
+          <div className="description">
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
+          </div>
+        </div>
 
-      {/* Location */}
-      <div className="location-box">
-        <div className="location">New York City, US</div>
-        <div className="date">Sunday 7th March 2021</div>
+        {data.name !== undefined &&
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+              <p>Wind Speed</p>
+            </div>
+          </div>
+        }
+
+
+
       </div>
-
-      {/* Weather Box */}
-      <div className="weather-box">
-        <div className="temp">15°C</div>
-        <div className="weather">Sunny</div>
-      </div>
-
-      </header>
     </div>
   );
 }
+
 
 export default App;
